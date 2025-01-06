aa9a03dcf4da4c6435fae74f68f4163df94b0210
aleksandr258 <aleksandralekseev258@gmail.com>
2025-01-06 18:44:40 +0900

    7.5

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -17,11 +17,15 @@ function JournalForm({onSubmit}) {
 	const [formValidState, setFormValidState] = useState(INITIAL_STATE);
 
 	useEffect(() => {
+		let timerId; 
 		 if (!formValidState.date || !formValidState.post || !formValidState.title || !formValidState.tag){
-			setTimeout(() => {
+			timerId = setTimeout(() => {
 				 setFormValidState(INITIAL_STATE);
-			});
+			}, 2000);
 		 }
+		 return (() => {
+			 clearTimeout(timerId);
+		 });
 	}, [formValidState]);
 
 	const addJournalItem = (e) => {
